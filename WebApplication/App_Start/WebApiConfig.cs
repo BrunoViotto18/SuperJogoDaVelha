88cb446caeb37c4be3ebc7564d623bdf74d229da
Bruno Viotto <brunoviottosantos@gmail.com>
2022-04-19 11:07:54 -0300

    Upload de arquivos

---
+++ WebApplication/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApplication
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuração e serviços de API Web
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // Rotas de API Web
+            config.MapHttpAttributeRoutes();
+
+            SuperJogoDaVelhaEntities entities = new SuperJogoDaVelhaEntities(); 
+
+            config.Routes.MapHttpRoute(
+                name: "User",
+                routeTemplate: "api/login/{username}/{senha}",
+                new
+                {
+                    controller = "User",
+                    nome = string.Empty,
+                    senha = string.Empty
+                }
+            );
+        }
+    }
+}
